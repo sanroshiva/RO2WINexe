a286d11c3d9f629735995f8564f67146fb7f6793
sanroshiva <sanroshiva@gmail.com>
2018-01-29 03:28:40 +0800

    Update SYRO_dl.bat

--- SYRO_dl.bat
+++ SYRO_dl.bat
@@ -1,3 +1,3 @@
-wget -n https://raw.githubusercontent.com/sanroshiva/RO2WINexe/master/SYRO180123.exe
+wget -N https://raw.githubusercontent.com/sanroshiva/RO2WINexe/master/SYRO180123.exe
 call SYRO180123.exe
 del SYRO_dl.bat
