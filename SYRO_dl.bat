753c0d5bdd8441f96cd5730b5cf7661071c9275a
sanroshiva <sanroshiva@gmail.com>
2018-01-29 20:27:09 +0800

    Update SYRO_dl.bat

--- SYRO_dl.bat
+++ SYRO_dl.bat
@@ -1,11 +1,8 @@
 @echo off
-echo SYR‧GNJOY TwRO修正檔_目前最新版本為　SYRO180123
-echo ====================================================
-echo 請按任意鍵開始下載最新版本，下載完會跳出安裝視窗
-echo 已擁有此版本或不想下載者，請點選視窗右上角的╳(關閉)鈕，或按Ctrl+C取消
-echo 最新版本可能是維修前版本，請注意版本日期(除非已註明可沿用)
+echo Latest Version→SYRO180123
+echo =======================
+echo Press any key to download the latest version
 pause
-echo 正在下載...
 wget -N https://raw.githubusercontent.com/sanroshiva/RO2WINexe/master/SYRO180123.exe
 call SYRO180123.exe
 del SYRO180123.exe
