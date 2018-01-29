6bda26fb8d2709082fd6ee01f1a9bdc478a04b8c
sanroshiva <sanroshiva@gmail.com>
2018-01-29 20:45:22 +0800

    Update SYRO_dl.bat

--- SYRO_dl.bat
+++ SYRO_dl.bat
@@ -1,7 +1,9 @@
 @echo off
-echo Latest Version      SYRO180123
-echo =======================
+echo ==============================================
+echo Latest Version            SYRO180123
+echo 
 echo Press any key to download the latest version
+echo ==============================================
 pause
 wget -N https://raw.githubusercontent.com/sanroshiva/RO2WINexe/master/SYRO180123.exe
 call SYRO180123.exe
