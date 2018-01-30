50fd7fbb7246ec620fc7078096ef457f22b00af1
sanroshiva <sanroshiva@gmail.com>
2018-01-30 08:23:17 +0800

    Update SYRO_dl.bat

--- SYRO_dl.bat
+++ SYRO_dl.bat
@@ -1,7 +1,7 @@
 @echo off
 echo ==============================================
-echo Latest Version            SYRO180123
-echo ---
+echo Latest Version            SYRO180123 (2018/01/23 00:00:00)
+echo 
 echo Press any key to download the latest version
 echo ==============================================
 pause
