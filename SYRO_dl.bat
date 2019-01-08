7a62614e1edf9a0dfb1f7db8787278b1ca99e42c
sanroshiva <sanroshiva@gmail.com>
2019-01-08 22:21:28 +0800

    Add files via upload

--- SYRO_dl.bat
+++ SYRO_dl.bat
@@ -1,11 +1,11 @@
 @echo off
 echo ==============================================
-echo Latest Version            SYRO181226 (2018/12/26 20:38)
+echo Latest Version            SYRO190108 (2019/01/08 22:18)
 echo ---
 echo Press any key to download the latest version
 echo ==============================================
 pause
-wget -N https://raw.githubusercontent.com/sanroshiva/RO2WINexe/master/GNJOY_2018/SYRO181226.exe
-call SYRO181226.exe
-del SYRO181226.exe
+wget -N https://raw.githubusercontent.com/sanroshiva/RO2WINexe/master/GNJOY_2019/SYRO190108.exe
+call SYRO190108.exe
+del SYRO190108.exe
 del SYRO_dl.bat
